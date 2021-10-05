ae7766d13af07b5bb6d519a9bfcd208cd5144335
Filipe Oliveira <hi@fmoliveira.dev>
2021-10-04 23:41:58 -0300

    Flatten rarity data

--- packages/rarity-tools/src/index.mjs
+++ packages/rarity-tools/src/index.mjs
@@ -2,9 +2,11 @@ import getFileData from "./lib/getFileData.mjs";
 import getTokenData from "./lib/getTokenData.mjs";
 import getTraitsFrequency from "./lib/getTraitsFrequency.mjs";
 import calculateRarityScores from "./lib/calculateRarityScores.mjs";
+import flattenRarityData from "./lib/flattenRarityData.mjs";
 
 const tokenData = getTokenData(getFileData(process.argv[2]));
 const traitsFrequency = getTraitsFrequency(tokenData);
 const rarityScores = calculateRarityScores(tokenData, traitsFrequency);
+const flattenedRarityData = flattenRarityData(rarityScores);
 
-console.log(rarityScores);
+console.log(flattenedRarityData);
---
+++ packages/rarity-tools/src/lib/flattenRarityData.mjs
@@ -0,0 +1,33 @@
+/**
+ * Flatten rarity scores for simpler exporting.
+ */
+export default function flattenRarityData(rarityScores) {
+	if (!rarityScores) {
+		displayError(`
+    ERROR: Missing argument for flattening rarity data!
+    `);
+	}
+
+	// initialize headers
+	const headers = ["tokenId"];
+	const traitTypes = Object.keys(rarityScores[0].traitsList); // DANGER: may crash if the token traits are not consistent
+	traitTypes.forEach((trait) => {
+		headers.push(trait, trait); // push trait twice to store name and rarity score
+	});
+	headers.push("totalRarity");
+
+	// flattens rows
+	const flattenedRows = rarityScores.map(
+		({ tokenId, traitsList, traitsRarity, totalRarity }) => {
+			const row = [tokenId];
+			traitTypes.forEach((trait) => {
+				row.push(traitsList[trait], traitsRarity[trait]);
+			});
+			row.push(totalRarity);
+
+			return row;
+		},
+	);
+
+	return [headers, ...flattenedRows];
+}
